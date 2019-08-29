4dbea7d87047e1e7d5bcaf92da656b6ce00c6d59
Weston Straw <weston.straw@gmail.com>
2019-08-29 17:59:51 -0500

    Libraries and initial push

---
+++ src/TestSetup.java
@@ -0,0 +1,7 @@
+import org.lwjgl.Version;
+
+public class TestSetup {
+    public static void main(String[] args) {
+        System.out.println("LWJGL Version " + Version.getVersion() + " is working.");
+    }
+}
