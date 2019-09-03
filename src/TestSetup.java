9cd1341c2c1a60fb717d5cae2e3a8c86fe7f44cb
Weston Straw <weston.straw@gmail.com>
2019-09-02 21:23:33 -0500

    Update of main with test code to ensure library is working as intended

--- src/TestSetup.java
+++
@@ -1,7 +0,0 @@
-import org.lwjgl.Version;
-
-public class TestSetup {
-    public static void main(String[] args) {
-        System.out.println("LWJGL Version " + Version.getVersion() + " is working.");
-    }
-}
