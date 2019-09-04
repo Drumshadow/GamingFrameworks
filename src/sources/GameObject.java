c2f0e8a73c541f9662bfe124cce053d17bf5b548
Ashley Roesler <ashley_roesler@baylor.edu>
2019-09-04 17:34:16 -0500

    moved obj class

--- src/sources/GameObject.java
+++ src/sources/GameObject.java
@@ -4,11 +4,14 @@
  */
 
 import java.awt.Rectangle;
+import java.awt.geom.Area;
 
 // represents a generic game object
 public class GameObject {
 
     private String objName;
+
+    private String spritePath;
 
     private boolean hasGravity;
     private boolean canCollide;
@@ -26,21 +29,67 @@ public class GameObject {
     private int xSpeed;
     private int ySpeed;
 
-    private Rectangle hitBox;
+    private Rectangle boundBox;
 
-    // TODO: default constructor
-    // TODO: check collision
-    public Boolean collisionCheck(GameObject other) {
+    /*==================================================
+                     Initialization
+    ==================================================*/
 
+    public GameObject() {
+        this.objName = "Generic Object";
+
+        this.hasGravity = false;
+        this.canCollide = false;
+
+        this.x = 0;
+        this.y = 0;
+
+        this.width = 0;
+        this.height = 0;
+
+        this.xSpeed = 0;
+        this.ySpeed = 0;
+
+        this.boundBox = null;
+    }
+
+    // TODO: create bounding box
+    // TODO: load sprite
+
+    /*==================================================
+                        Movement
+    ==================================================*/
+
+    public void moveX() {
+        this.x =+ xSpeed;
+    }
+
+    public void moveY() {
+        this.y =+ ySpeed;
+    }
+
+    // TODO: jump
+
+    /*==================================================
+                        Collision
+    ==================================================*/
+
+    // checks if two bounding boxes have collided
+    public Boolean basicCollisionCheck(GameObject other) {
+
+        // make sure objects can be collided with
         if (!this.canCollide || !other.canCollide)
             return false;
 
-        return this.hitBox.intersects(other.hitBox);
+        return this.boundBox.intersects(other.boundBox);
     }
 
+    // TODO: more accurate collision check
 
-    // TODO: fill in rectangle so object can be drawn
-    // TODO: movement controls
+    /*==================================================
+                   Setters and Getters
+    ==================================================*/
+
     // TODO: setters and getters
 
 }
