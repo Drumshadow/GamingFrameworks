ab0e540840fa8b4a3e73f9d29a26aad23a459af1
Ashley Roesler <ashley_roesler@baylor.edu>
2019-09-06 11:51:06 -0500

    added room class, added obj setters, getters, and equals

--- src/sources/GameObject.java
+++ src/sources/GameObject.java
@@ -1,9 +1,9 @@
 /*
  * author: Ashley Roesler
- * last updated: 09/03/19
  */
 
 import java.awt.Rectangle;
+import java.util.Objects;
 
 // represents a generic game object
 public class GameObject {
@@ -39,7 +39,10 @@ public class GameObject {
     ==================================================*/
 
     public GameObject() {
-        this.objName = "Generic Object";
+        this.objName = "Empty Object";
+        this.spritePath = null;
+
+        // TODO: default sprite when none is given?
 
         this.hasGravity = false;
         this.canCollide = false;
@@ -56,6 +59,15 @@ public class GameObject {
         this.boundBox = null;
     }
 
+    // TODO: non generic constructor?
+
+    // sets object's sprite
+    // also crops sprite, making sure there is no empty space around the sprite
+    public void loadSprite(String spritePath) {
+
+
+    }
+
     // TODO: create bounding box
     // TODO: load sprite
     // TODO: draw
@@ -64,7 +76,27 @@ public class GameObject {
                         Movement
     ==================================================*/
 
-    public void move() {
+    // moves objects and performs collision detection
+    public void move(GameObject[] objs) {
+
+        // check collision
+        if (this.canCollide) {
+
+            for (GameObject o : objs) {
+
+                // don't collide with self and check if can collide
+                if (this.equals(o))
+                    continue;
+
+                // check if o can collide
+                if (!o.canCollide)
+                    continue;
+
+
+            }
+        }
+
+        // move if able
 
     }
 
@@ -105,10 +137,133 @@ public class GameObject {
         return futureBox.intersects(other.boundBox);
     }
 
+    // checks future collision in both x and y directions
+    public boolean futureCollisionCheck(GameObject other) {
+
+        // make temporary bounding box for future position
+        Rectangle futureBox = new Rectangle(this.x + this.xSpeed,
+                this.y + this.ySpeed, this.width, this.height);
+
+        return futureBox.intersects(other.boundBox);
+    }
+
     /*==================================================
                    Setters and Getters
     ==================================================*/
 
-    // TODO: setters and getters
+    // TODO: add value validation
 
+    public void setObjName(String o) {
+        this.objName = o;
+    }
+
+    public String getObjName() {
+        return this.objName;
+    }
+
+    public void setSpritePath(String s) {
+        this.spritePath = s;
+    }
+
+    public String getSpritePath() {
+        return this.spritePath;
+    }
+
+    public void setHasGravity(boolean g) {
+        this.hasGravity = g;
+    }
+
+    public boolean getHasGravity() {
+        return this.hasGravity;
+    }
+
+    public void setCanCollide(boolean c) {
+        this.canCollide = c;
+    }
+
+    public boolean getCanCollide() {
+        return this.canCollide;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setWidth(int w) {
+        this.width = w;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public void setHeight(int h) {
+        this.height = h;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void setXSpeed(int s) {
+        this.xSpeed = s;
+    }
+
+    public int getXSpeed() {
+        return this.xSpeed;
+    }
+
+    public void setYSpeed(int s) {
+        this.ySpeed = s;
+    }
+
+    public int getYSpeed() {
+        return this.ySpeed;
+    }
+
+    public void setBoundBox(Rectangle b) {
+        this.boundBox = b;
+    }
+
+    public Rectangle getBoundBox() {
+        return this.boundBox;
+    }
+
+    /*==================================================
+                      Miscellaneous
+    ==================================================*/
+
+    // generates object's hashcode for equality check
+    public int hashcode() {
+        return Objects.hash(this.objName, this.spritePath, this.hasGravity,
+                this.canCollide, this.x, this.y, this.width, this.height,
+                this.xSpeed, this.ySpeed, this.boundBox);
+    }
+
+    // checks if two objects are the same
+    public boolean equals(Object other) {
+
+        // Check if they are the same object
+        if (this == other)
+            return true;
+
+        // Check if null or if same class
+        if (other == null || this.getClass() != other.getClass())
+            return false;
+
+        // Check class members
+        return (this.hashcode() == other.hashCode());
+    }
 }
---
+++ src/sources/GameRoom.java
@@ -0,0 +1,14 @@
+/*
+ * author: Ashley Roesler
+ */
+
+public class GameRoom {
+
+    private String roomName;
+    private String backgroundPath;
+
+    private GameObject[] allObjects;
+
+    // TODO: all the functions..... which functions? no clue
+    // TODO: make function that passes allObjects to object (move() function)
+}
