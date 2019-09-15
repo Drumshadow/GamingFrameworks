e173fb1ec6970e0bb527019d61f3552fe76cd0fc
Ashton <Stevie_Damrel@baylor.edu>
2019-09-15 17:24:57 -0500

    FPS is capped at 60fps and it printed live into terminal Looking into how to print it onto the window, cap can easily be changed by changing the frame_cap var

---
+++ src/sources/FPS.java
@@ -0,0 +1,33 @@
+package sources;
+
+import org.lwjgl.*;
+
+import java.awt.*;
+
+public class FPS {
+    private int lastFPS;
+
+    /**
+     * Get the time in milliseconds
+     *
+     * @return The system time in milliseconds
+     */
+    public static double getTime() {
+        return (double) System.nanoTime() / (double) 1000000000L;
+    }
+
+    public void start() {
+        //some startup code
+        lastFPS = (int) getTime(); //set lastFPS to current Time
+    }
+
+    public void updateFPS() {
+        int fps = 0;
+        if (getTime() - lastFPS > 1000) {
+            System.out.println("FPS" + fps);
+            fps = 0; //reset the FPS counter
+            lastFPS += 1000; //add one second
+        }
+        fps++;
+    }
+}
