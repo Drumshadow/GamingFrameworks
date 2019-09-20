a1a6bbd02e98dfa2d1040f37b9d1a088f80a90bb
Cody <Cody_Dowell@baylor.edu>
2019-09-20 02:12:51 -0500

    Just a lil thing

--- src/sources/GameLoop.java
+++ src/sources/GameLoop.java
@@ -24,7 +24,7 @@ import static org.lwjgl.opengl.GL11.*;
 
 
 public class GameLoop {
-
+    // Hello!!!
     // window handle
     private InitWindow newWindow = new InitWindow();
     private InputList inputs = new InputList();
